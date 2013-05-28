0340b4de2c7a0461206c3025b31e579445f818c7
CatharticMonkey <joneseh25+github@gmail.com>
2013-05-28 15:43:36 +0100

    Added XCode files

---
+++ Webapps/Webapps/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Webapps
+//
+//  Created by Richard Jones on 27/05/2013.
+//  Copyright (c) 2013 Richard Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
