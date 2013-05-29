96e71e9775f4e8a86aa2b3cf4ea2260bcc40a566
Richard Jones <rj1411@vm-shell3.doc.ic.ac.uk>
2013-05-29 16:56:44 +0100

    Added php scripts for writing to the database

--- insert.php
+++ insert.php
@@ -1,29 +1,14 @@
-<!DOCTYPE html>
-<head>
-<title>Insert data to PostgreSQL with php - creating a simple web application</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<style>
-li {
-list-style: none;
-}
-</style>
-</head>
+#!/usr/bin/php
+<html>
 <body>
-<h2>Enter username</h2>
-<ul>
-<form name="insert" action="insert.php" method="POST" >
-
-<li>Username:</li><li><input type="text" name="username" /></li>
-
-<li><input type="submit" /></li>
-</form>
-</ul>
-</body>
-</html>
-
-
 <?php
 $db = pg_connect("host=db.doc.ic.ac.uk port=5432 dbname=g1227105_u user=g1227105_u password=4TVIDKYHVZ");
-$query = "INSERT INTO Users VALUES ('$_POST[username]','$_POST[user_name]'";
-$result = pg_query($query);
-?> 
+$query = "INSERT INTO \"Users\" VALUES ('$_POST[username]')";
+$result = pg_query($db, $query);
+$query = "SELECT * FROM \"Users\"";
+$result = pg_query($db, $query);
+$row = pg_fetch_row($result, 0);
+echo $row[0]
+?>
+</body>
+</html>
