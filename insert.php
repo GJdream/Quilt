0fa6a19f4ae929a9efedeff59349b57b88070e2b
e-vincent <eleanor.vincent@ntlworld.com>
2013-05-30 12:22:57 +0100

    Small edit to reflect current DB structure

--- insert.php
+++ insert.php
@@ -2,12 +2,14 @@
 <html>
 <body>
 <?php
-$db = pg_connect("host=db.doc.ic.ac.uk port=5432 dbname=g1227105_u user=g1227105_u password=4TVIDKYHVZ");
-$query = "INSERT INTO \"Users\" VALUES ('$_POST[username]')";
+$db     = pg_connect("host=db.doc.ic.ac.uk port=5432 dbname=g1227105_u user=g1227105_u password=4TVIDKYHVZ");
+
+$query  = "INSERT INTO \"Users\" (user_name) VALUES ('$_POST[username]')";
 $result = pg_query($db, $query);
-$query = "SELECT * FROM \"Users\"";
+$query  = "SELECT * FROM \"Users\"";
 $result = pg_query($db, $query);
-$row = pg_fetch_row($result, 0);
+
+$row    = pg_fetch_row($result, 0);
 echo $row[0]
 ?>
 </body>
