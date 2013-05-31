ece755c989e1cea25826698ac16dc6e698810aac
Richard Jones <joneseh25@gmail.com>
2013-05-31 02:34:02 +0100

    Added stuff for reading from POST method. Most of today has been working out methods. Tomorrow will be writing code to deal with requests (and testing with telnet).

--- server/listen.php
+++ server/listen.php
@@ -1,10 +1,27 @@
 #!/usr/bin/php
-
 <html>
 <head/>
 <body>
 <?php
+
 $db     = pg_connect("host=db.doc.ic.ac.uk port=5432 dbname=g1227105_u user=g1227105_u password=4TVIDKYHVZ");
+
+echo $_SERVER[REQUEST_METHOD];
+echo "<br/>";
+if($_SERVER[REQUEST_METHOD] === "POST")
+{
+    echo "HERE";
+    if($_POST["user_name"] || $_POST["password"])
+    {
+        echo $_POST['user_name'];
+        echo "<br/>";
+        echo $_POST['password'];
+        echo "<br/>";
+    }
+}
+
+if(false)
+{
 
 $query  = "INSERT INTO \"Users\" (user_name) VALUES ('$_POST[username]')";
 $result = pg_query($db, $query);
@@ -14,6 +31,7 @@ $result = pg_query($db, $query);
 while($row    = pg_fetch_array($result))
 {
     echo $row[0] . "<br />";
+}
 }
 
 ?>
