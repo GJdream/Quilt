6d460ef02948d0724b0931dd43321f4c5014ae75
e-vincent <eleanor.vincent@ntlworld.com>
2013-06-01 14:50:41 +0100

    Added method for post resizing. Bookmarks table updated to contain post_id column as extra assurance that the correct post is edited. post_id is a unique column, not set as primary key.

--- server/bookmark.php
+++ server/bookmark.php
@@ -5,6 +5,7 @@
     {
       global $db;
 
+      $postid  = $_POST[post_id];
       $owner   = $_POST[owner];
       $link    = $_POST[url];
       $pheight = $_POST[p_height];
@@ -14,12 +15,28 @@
       $tags   = implode("','", $_POST[tags]);
       $tagarr = "ARRAY['" . $tags . "']";
 
-      $query  = "INSERT INTO \"Bookmarks\" (owner, url, p_height, p_width, tags) " .
-                "VALUES ('$owner', '$link', '$pheight', '$pwidth', ARRAY['$tags'])";
+      $query  = "INSERT INTO \"Bookmarks\" (post_id, owner, url, p_height, p_width, tags) " .
+                "VALUES ('$postid', '$owner', '$link', '$pheight', '$pwidth', ARRAY['$tags'])";
       echo $query;
 
       $result = pg_query($db, $query);
       echo $result;
+    }
+
+  function resizeBookmark()
+    {
+      global $db;
+
+      $postid  = $_POST[post_id];
+      $owner   = $_POST[owner];
+      $link    = $_POST[url];
+      $pheight = $_POST[p_height];
+      $pwidth  = $_POST[p_width];
+
+      $query  = "UPDATE \"Bookmarks\" " .
+                "SET p_height = 'pheight', p_width = 'pwidth' " .
+                "WHERE owner = '$owner' AND post_id = '$postid'";
+      $result = pg_query($db, $query);
     }
 
 ?>
