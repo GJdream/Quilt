b7c4525a2368d2fc79c0e4da39c38c1e95e32ddd
CatharticMonkey <joneseh25+github@gmail.com>
2013-06-02 12:28:46 +0100

    Added the start of a Networking client class.

---
+++ Webapps/Webapps/NetworkClient.h
@@ -0,0 +1,13 @@
+//
+//  NetworkClient.h
+//  Webapps
+//
+//  Created by Richard Jones on 01/06/2013.
+//  Copyright (c) 2013 Team Awesome. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkClient : NSObject
+
+@end
---
+++ Webapps/Webapps/NetworkClient.m
@@ -0,0 +1,13 @@
+//
+//  NetworkClient.m
+//  Webapps
+//
+//  Created by Richard Jones on 01/06/2013.
+//  Copyright (c) 2013 Team Awesome. All rights reserved.
+//
+
+#import "NetworkClient.h"
+
+@implementation NetworkClient
+
+@end
