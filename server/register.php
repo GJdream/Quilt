086d11af8b49cb6db8e6a0a734204b4977fa2c90
e-vincent <eleanor.vincent@ntlworld.com>
2013-06-05 13:34:49 +0100

    Completed unregister function. Still requires testing thoroughly

--- server/register.php
+++ server/register.php
@@ -1,9 +1,8 @@
 <?php
-  
+  global $db;
+
   function register()
     {
-      global $db;
-
       $username = $_POST['username'];
       $password = sha1($_POST['password']);
         // md5 and sha1 are not good hash functions to use {md5 moreso}
@@ -30,5 +29,104 @@
         }
 
       // insert successful
+    }
+
+  function unregister()
+    {
+      $username = $_POST['username'];
+      $password = sha1($_POST['password']);
+        // ask the user for their password as insurance that they do want to unregister
+
+      // if they are unregistering we want to remove all instances of them
+      // from the rest of the database because it makes no sense to referrence
+      // a user that no longer exists
+      $query   = "DELETE FROM \"Users\"" .
+                 "WHERE user_name = '$username' AND password = '$password'" . 
+                 "RETURNING user_id";
+      $result  = pg_query($db, $query);
+      echo $result;
+      $user_id = pg_fetch_result($result, 0);
+
+      $grpmem_tbl = "\"Group_Members\"";
+      $friend_tbl = "\"Friends\"";
+      $bmarks_tbl = "\"Bookmarks\"";
+
+      // run logout to clear session data before deleting information?
+
+      if(removeUserFrom($grpmem_tbl, $user_id) 
+         && removeUserFrom($friend_tbl, $user_id)
+         && removeUserFrom($bmarks_tbl, $user_id))
+        {
+          // if everything worked, goodbye {and thanks for all the fish}
+          return true;
+        }
+
+      return false;
+
+      // have not decided how to deal with group ownership yet
+      // the other members in the group might not want to lose group
+      // offer them option to promote a member to group owner?
+      // how to decide how to promote?
+    }
+
+  function removeUserFrom($table, $user_id)
+    {
+      if($table === "\"Group_Members\"")
+        {
+          // aliasing the correct column for use with common code at end of function
+          $query  = "SELECT group_id AS user_id " .
+                    "FROM $table";
+          $result = pg_query($db, $query);
+        }
+      else if($table === "\"Friends\"")
+        {
+          // Friends has a user_id column and a friend_id column
+          // we can't alias friend_id to user_id as the column exists already
+          // here is the deletion case for friend_id
+          $query  = "DELETE FROM $table" .
+                    "WHERE friend_id = '$user_id'";
+          $result = pg_query($db, $query);
+
+          // the user_id deletion for Friends will occur correctly
+          // using the common code at the end of the function since
+          // the column exists as required
+        }
+      else if($table === "\"Bookmarks\"")
+        {
+          // as the user no longer exists we will be deleting all of their posts
+          // this will require us to remove those entries from the Bookmark_Visibility table
+          // and remove all associated tags for that post from the Tags table
+          // as such, we must build an array of the post_ids affected by this
+          $query   = "SELECT post_id FROM $table" .
+                     "WHERE owner_id = '$user_id'";
+          $result  = pg_query($db, $query);
+
+          $post_ids = pg_fetch_array($result);
+
+          // deleting post data
+          foreach($post_ids as $post_id)
+            {
+              $query  = "DELETE FROM \"Bookmark_Visibility\"" .
+                        "WHERE post_id = '$post_id'";
+              $result = pg_query($db, $query);
+
+              $query  = "DELETE FROM \"Tags\"" .
+                        "WHERE post_id = '$post_id'";
+              $result = pg_query($db, $query);
+            }
+
+          // here is the column alias for the common query deletion below
+          $query   = "SELECT owner_id AS user_id " .
+                     "FROM $table";
+          $result  = pg_query($db, $query);
+        }
+
+      $query  = "DELETE FROM $table" .
+                "WHERE user_id = '$user_id'";
+      $result = pg_query($db, $query); 
+
+      // work out better test for success
+      // may require a common function for testing result success
+      return true;     
     }
 ?>
