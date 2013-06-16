3c863a701c19f3b6504a5e1c5e6ab84eb1d7e9fe
e-vincent <eleanor.vincent@ntlworld.com>
2013-06-16 19:47:08 +0100

    Style Edit

--- server/listen.php
+++ server/listen.php
@@ -59,10 +59,7 @@
       else if($_POST['action'] === "change_password")
         updatePassword();
       else if($_POST['action'] === "new_bookmark")
-      {
         createBookmark();
-        updateBookmarkPicture();
-      }
       else if($_POST['action'] === "remove_bookmark")
         destroyBookmark();
       else if($_POST['action'] === "new_tag")
