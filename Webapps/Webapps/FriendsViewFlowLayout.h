e70303569e92e6f4c6a510d6086eb7847ec170ad
Goldsack <bkg11@cider06.doc.ic.ac.uk>
2013-06-17 12:00:06 +0100

    Friends padding... again

---
+++ Webapps/Webapps/FriendsViewFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  FriendsViewFlowLayout.h
+//  Quilt
+//
+//  Created by Goldsack, Briony on 17/06/2013.
+//  Copyright (c) 2013 Team Awesome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsViewFlowLayout : UICollectionViewFlowLayout
+
+@end
